838d445b730efb819c887df1c0ef1851ec614625
johnloberger <johnloberger@gmail.com>
2020-06-15 17:59:08 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,12 @@
+class SecretsController < ApplicationController 
+
+  def show 
+    if session.include? :name
+      render :show 
+    else 
+      redirect_to '/login'
+    end 
+  end 
+
+
+end 
